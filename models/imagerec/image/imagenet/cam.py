1a490400ff1291cb2b74c6d6f747684ad9ce8137
shaktichetan <41295792+shaktichetan@users.noreply.github.com>
2018-07-19 21:02:56 +0530

    Add files via upload

---
+++ models/imagerec/image/imagenet/cam.py
@@ -0,0 +1,28 @@
+import cv2
+
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+if vc.isOpened(): 
+    rval, frame = vc.read()
+else:
+    rval = False
+
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    #if key == 0x63 or key == 0x43:
+    	
+   	#	 cv2.SaveImage("images.png",frame) 
+
+    if key == 27: # exit on ESC
+        break
+
+cv2.destroyWindow("preview")
+
+
+
+
+
+
